d76286c13aac3a3795784e88cee24a2c91981e63
GMaysa <maysaputra0205@gmail.com>
2023-06-06 21:39:22 +0800

    add: link favicon and title

--- src/components/Layout/NormalLayout.tsx
+++ src/components/Layout/NormalLayout.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
 import Navbar from "../Header/Navbar";
 import Footer from "../Footer/Footer";
+import Head from "next/head";
 
 // TODO : Add Navbar and Footer
+
 
 interface NormalLayoutProps {
   children: React.ReactNode;
@@ -12,6 +14,10 @@ interface NormalLayoutProps {
 const NormalLayout: FC<NormalLayoutProps> = ({ children, pageTitle }) => {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/logo-colored.svg" sizes="any" />
+        <title>{pageTitle}</title>
+      </Head>
       <Navbar/>
       <div className="w-full mx-auto max-w-screen-2xl bg-white">{children}</div>
       <Footer/>
