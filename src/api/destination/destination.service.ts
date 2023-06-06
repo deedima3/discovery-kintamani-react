d3bdd7eab09796ff38185841094763cf464e9fc8
GMaysa <maysaputra0205@gmail.com>
2023-06-06 21:37:53 +0800

    add: query destination

--- src/api/destination/destination.service.ts
+++ src/api/destination/destination.service.ts
@@ -12,6 +12,44 @@ import {
 import { graphQLClient } from "@/utils/query";
 import { gql } from "graphql-request";
 
+export const getDestinations = async () => {
+  const query = gql`
+  query MyQuery {
+    destinations(where: {isFeatured: true}) {
+      alwaysOpen
+      category
+      closeTime
+      coordinate {
+        latitude
+        longitude
+      }
+      id
+      images {
+        ... on Image {
+          id
+          alt
+          image {
+            width
+            url
+            height
+          }
+        }
+      }
+      isFeatured
+      location
+      openTime
+      publishedAt
+      shortDescription
+      slug
+      stage
+      title
+    }
+  }
+  `
+  const { destinations } = await graphQLClient.request(query) as ApiDataResponse<DestinationQueryData[]>
+  return destinations
+}
+
 const getAllDestinationPaginated = async (params: PaginatedQueryParams) => {
   const query = gql`
     query DestinationQuery($search: String, $limit: Int, $offset: Int) {
